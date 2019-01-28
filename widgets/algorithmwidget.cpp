7e60238fc31c997c2eff2cc73fa21ac73ff6117e
Asset34 <artemiy.kashintsev@gmail.com>
2019-01-28 18:20:32 +0300

    AlgorithmWidget: implement class

---
+++ widgets/algorithmwidget.cpp
@@ -0,0 +1,68 @@
+#include "algorithmwidget.hpp"
+
+#include <QVBoxLayout>
+
+#include "algorithmselector.hpp"
+#include "parameterlistwidget.hpp"
+
+AlgorithmWidget::AlgorithmWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    m_algorithmSelector = new AlgorithmSelector;
+    m_parameterListWidget = new ParameterListWidget;
+
+    m_layout = new QVBoxLayout;
+    m_layout->setContentsMargins(0, 0, 0, 0);
+    m_layout->addWidget(m_algorithmSelector);
+    m_layout->addWidget(m_parameterListWidget);
+
+    setLayout(m_layout);
+
+    connect(
+        m_algorithmSelector,
+        &AlgorithmSelector::algorithmSelected,
+        this,
+        &AlgorithmWidget::updateParameters
+        );
+}
+
+int AlgorithmWidget::getCount() const
+{
+    return m_algorithmSelector->getCount();
+}
+
+Algorithm *AlgorithmWidget::getAlgorithm(int index) const
+{
+    return m_algorithmSelector->getAlgorithm(index);
+}
+
+Algorithm *AlgorithmWidget::getCurrentAlgorithm() const
+{
+    return m_algorithmSelector->getCurrentAlgorithm();
+}
+
+void AlgorithmWidget::addAlgorithm(Algorithm &algorithm)
+{
+    m_algorithmSelector->addAlgorithm(algorithm);
+}
+
+void AlgorithmWidget::removeAlgorithm(Algorithm &algorithm)
+{
+    m_algorithmSelector->removeAlgorithm(algorithm);
+}
+
+void AlgorithmWidget::removeAlgorithm(int index)
+{
+    m_algorithmSelector->removeAlgorithm(index);
+}
+
+void AlgorithmWidget::selectAlgorithm(int index)
+{
+    m_algorithmSelector->selectAlgorithm(index);
+    updateParameters(m_algorithmSelector->getCurrentAlgorithm());
+}
+
+void AlgorithmWidget::updateParameters(Algorithm *algorithm)
+{
+    m_parameterListWidget->updateWith(*algorithm);
+}
---
+++ widgets/algorithmwidget.hpp
@@ -0,0 +1,45 @@
+#ifndef ALGORITHMWIDGET_HPP
+#define ALGORITHMWIDGET_HPP
+
+#include <QWidget>
+
+#include "../core/algorithm.hpp"
+
+class QVBoxLayout;
+class AlgorithmSelector;
+class ParameterListWidget;
+
+class AlgorithmWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AlgorithmWidget(QWidget *parent = nullptr);
+
+    int getCount() const;
+
+    Algorithm *getAlgorithm(int index) const;
+    Algorithm *getCurrentAlgorithm() const;
+
+    void addAlgorithm(Algorithm &algorithm);
+    void removeAlgorithm(Algorithm &algorithm);
+    void removeAlgorithm(int index);
+
+public slots:
+    void selectAlgorithm(int index);
+
+private:
+    QVBoxLayout *m_layout;
+
+    AlgorithmSelector *m_algorithmSelector;
+    ParameterListWidget *m_parameterListWidget;
+
+private slots:
+    void updateParameters(Algorithm *algorithm);
+
+signals:
+    void algorithmSelected(Algorithm *algorithm);
+
+};
+
+#endif // ALGORITHMWIDGET_HPP
